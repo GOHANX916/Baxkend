d494b1de73cf67f2f4eb4ab7fa3fc8465c320d52
GOHANX916 <mohansita4@gmail.com>
2025-02-12 12:57:18 +0530

    Added self-ping to keep server alive

--- server.js
+++ server.js
@@ -11,28 +11,39 @@ const wss = new WebSocketServer({ server });
 app.use(cors());
 app.get("/", (req, res) => res.send("WebSocket Server is running"));
 
+// Store connected clients
+const clients = new Set();
+
 // WebSocket Connection
 wss.on("connection", (ws) => {
     console.log("A user connected");
+    clients.add(ws);
 
     ws.on("message", (message) => {
         console.log("Received:", message.toString());
 
-        // Broadcast message to all clients **except the sender**
-        wss.clients.forEach((client) => {
+        // Convert message to object
+        const msgObj = JSON.parse(message);
+        msgObj.seen = false; // Initially, the message is not seen
+
+        // Broadcast message to all clients except sender
+        clients.forEach((client) => {
             if (client !== ws && client.readyState === 1) {
-                client.send(message.toString());
+                client.send(JSON.stringify(msgObj));
             }
         });
     });
 
-    ws.on("close", () => console.log("A user disconnected"));
+    ws.on("close", () => {
+        console.log("A user disconnected");
+        clients.delete(ws);
+    });
 });
 
-// **Self-ping every 5 minutes to keep the server alive**
+// Keep Server Alive
 setInterval(() => {
     require("http").get("https://baxkend.onrender.com/");
     console.log("Self-pinging to keep alive...");
-}, 5 * 60 * 1000); // 5 minutes
+}, 5 * 60 * 1000);
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
