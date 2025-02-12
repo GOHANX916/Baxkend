ba433bdfa3ccfc3f7a142fdad34013e5cec2ef4f
GOHANX916 <mohansita4@gmail.com>
2025-02-12 12:48:36 +0530

    Added self-ping to keep server alive

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ wss.on("connection", (ws) => {
     ws.on("message", (message) => {
         console.log("Received:", message.toString());
 
-        // Broadcast message to all clients
+        // Broadcast message to all clients **except the sender**
         wss.clients.forEach((client) => {
-            if (client.readyState === 1) {
+            if (client !== ws && client.readyState === 1) {
                 client.send(message.toString());
             }
         });
